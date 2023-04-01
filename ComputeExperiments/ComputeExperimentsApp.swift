8e2911151d19a2e7e28b346c8a843f591128bef6
Chaitanya Shah <cheatu1319@gmail.com>
2023-04-01 11:07:37 -0700

    Initial Commit

---
+++ ComputeExperiments/ComputeExperimentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComputeExperimentsApp.swift
+//  ComputeExperiments
+//
+//  Created by Chait Shah on 4/1/23.
+//
+
+import SwiftUI
+
+@main
+struct ComputeExperimentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
